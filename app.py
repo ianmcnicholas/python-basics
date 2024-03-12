c865370f2f8f29b1c5f2311987362daa8856ae3c
ianmcnicholas <ianjmcnicholas@.com>
2024-03-12 08:55:13 +0000

    learning to work with excel sheets

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import math
-
+import random
 import ecommerce.shipping
+import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
 
 # print("Hello World")
 # print("o----")
@@ -177,3 +179,28 @@ greet_user(last_name="Smith")
 greet_user(last_name="Sumner", first_name="Hugo")
 
 ecommerce.shipping.find_cost()
+
+for i in range(3):
+    print(random.randint(10, 20))
+
+wb = xl.load_workbook("transactions.xlsx")
+sheet = wb["Sheet1"]
+a1 = sheet["A1"]
+
+for row in range(2, sheet.max_row + 1):
+    cell = sheet.cell(row, 3)
+    corrected_price = cell.value * 0.9
+    correct_price_cell = sheet.cell(row, 4)
+    correct_price_cell.value = corrected_price
+
+values = Reference(sheet,
+                   min_row=2,
+                   max_row=sheet.max_row,
+                   min_col=4,
+                   max_col=4)
+
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, "e2")
+
+wb.save("transactions2.xlsx")
